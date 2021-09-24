c9cb369fa8663e57b26b9fd280d24b035416d2e3
Rafael Cavalcanti <rccavalcanti@gmail.com>
2021-09-23 22:27:44 -0300

    Apply xresources patch

--- config.def.h
+++ config.def.h
@@ -3,9 +3,18 @@ static const char *user  = "nobody";
 static const char *group = "nogroup";
 
 static const char *colorname[NUMCOLS] = {
-	[INIT] =   "black",     /* after initialization */
-	[INPUT] =  "#005577",   /* during input */
+	[INIT] = "black",       /* after initialization */
+	[INPUT] = "#005577",    /* during input */
 	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/*
+ * Xresources preferences to load at startup
+ */
+ResourcePref resources[] = {
+		{ "color0",       STRING,  &colorname[INIT] },
+		{ "color4",       STRING,  &colorname[INPUT] },
+		{ "color1",       STRING,  &colorname[FAILED] },
 };
 
 /* treat a cleared input like a wrong password (color) */
